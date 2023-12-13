d7756215798bd1a0943f81a9fb7e452fed9086bc
DaniRiber <daniribeirolc@hotmail.com>
2023-12-13 00:45:27 -0300

    feat: Criando o cadastro de pet

--- src/app/demo/service/product.service.ts
+++ src/app/demo/service/product.service.ts
@@ -1,17 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Product } from '../api/product';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class ProductService {
 
-    constructor(private http: HttpClient) { }
+    private basePath = "products"
+
+    constructor(private http: HttpClient,
+        private db: AngularFireDatabase) { }
 
     getProductsSmall() {
         return this.http.get<any>('assets/demo/data/products-small.json')
             .toPromise()
             .then(res => res.data as Product[])
             .then(data => data);
+    }
+
+    createProduct(product: Product): any {
+        return this.db.list<Product>(this.basePath).push(product);
     }
 
     getProducts() {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBy355LTI-617-EV6lc6TvXke25f2BM62o",
+        authDomain: "crud-devops-d9c9a.firebaseapp.com",
+        databaseURL: "https://crud-devops-d9c9a-default-rtdb.firebaseio.com",
+        projectId: "crud-devops-d9c9a",
+        storageBucket: "crud-devops-d9c9a.appspot.com",
+        messagingSenderId: "1090833839347",
+        appId: "1:1090833839347:web:ffa4b6761e750ea15f3386",
+        measurementId: "G-3772E1VS81"
+    }
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
